5186f7d05aae8a43ccf55d1d6e75a2ffb88cbdf0
bexruzkarimov200023 <bexruzkarimov200023@gmail.com>
2026-05-14 11:43:37 +0500

    change text in course

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 // Beg'ruz
 export default function Course() {
   return (
-    <div className="bg-red-500 text-yellow-500">Course</div>
+    <div className="bg-red-500 text-yellow-500">Beg'zruz</div>
   )
 }
